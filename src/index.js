69feeaf4e32f69688941c52750e51963bb75f97f
Talal <talalahmad257@gmail.com>
2021-03-23 12:54:35 +0500

    Set up client

---
+++ src/index.js
@@ -0,0 +1,84 @@
+const
+  {
+    AkairoClient,
+    CommandHandler,
+    ListenerHandler,
+    InhibitorHandler,
+    MongooseProvider
+  } = require('discord-akairo'),
+  config = require('../config.json'),
+  { Schema, model } = require('mongoose');
+
+class BotClient extends AkairoClient {
+  constructor() {
+    super({
+      ownerID: config.owner
+    }, {
+      messageCacheMaxSize: 50,
+      messageCacheLifetime: 18e3,
+      messageSweepInterval: 18e3,
+      disableMentions: 'everyone',
+      presence: config.startupPresence
+    });
+
+    this.commandHandler = new CommandHandler(this, {
+      directory: './src/commands',
+      prefix: config.prefix,
+      aliasReplacement: /-/g,
+      commandUtil: true,
+      ignoreCooldown: msg => !msg.member || msg.member.hasPermission('ADMINISTRATOR') || this.isOwner(msg.author),
+      ignorePermissions: msg => !msg.member || msg.member.hasPermission('ADMINISTRATOR') || this.isOwner(msg.author),
+      argumentDefaults: {
+        prompt: Object.assign(config.commandPrompt, {
+          modifyStart: (msg, str) => `${msg.author}, ${str}\n\n${config.modifyCommandPrompt.start}`,
+          modifyRetry: (msg, str) => `${msg.author}, ${str}\n\n${config.modifyCommandPrompt.retry}`
+        }),
+        otherwise: config.strings.ArgumentParsingFailure
+      }
+    });
+    this.commandHandler.useListenerHandler(this.listenerHandler);
+
+    this.listenerHandler = new ListenerHandler(this, {
+      directory: './src/listeners'
+    });
+
+    this.inhibitorHandler = new InhibitorHandler(this, {
+      directory: './src/inhibitors'
+    });
+
+    this.listenerHandler.setEmitters({
+      commandHandler: this.commandHandler,
+      inhibitorHandler: this.inhibitorHandler,
+      listenerHandler: this.listenerHandler,
+      process
+    });
+
+    this.commandHandler.loadAll();
+    this.listenerHandler.loadAll();
+    this.inhibitorHandler.loadAll();
+
+    const schema = new Schema({
+      id: {
+        type: String,
+        required: true
+      },
+      settings: {
+        type: Object,
+        require: true
+      }
+    }, { minimize: false });
+  
+    const settingsModel = model('model', schema);
+    this.db = new MongooseProvider(settingsModel);
+  }
+
+  async login(token) {
+    await this.settings.init();
+    return super.login(token);
+  }
+  
+}
+
+const client = new BotClient();
+client.functions = require('./functions');
+client.login(process.env.TOKEN);
